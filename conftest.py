0c66dd020e547ab18b749cf6877b9156c2634346
Forwork89 <81515618+Forwork89@users.noreply.github.com>
2022-06-27 12:47:21 +0300

    S_F

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ from selenium.webdriver.chrome.options import Options
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
-    parser.addoption('--languages', action='store', default="en",
+    parser.addoption('--language', action='store', default="en",
                      help="Choose languages: en or fr")
 
 
