97eec9895921224f5b32ed3122dffa4526d83a12
Forwork89 <Sir.klinckin@yandex.ru>
2022-08-27 10:41:29 +0300

    54

--- login_page.py
+++ login_page.py
@@ -11,17 +11,17 @@ browser = webbrowser.Chrome()
 class LoginPage(BasePage):
     def should_be_login_page(self):
         self.should_be_login_url()
-        self.should_be_login_form()
-        self.should_be_register_form()
+        self.should_be_login_page()
+        self.should_be_register_page()
 
     def should_be_login_url(self):
         login_url = browser.current_url
         assert "login" in browser.current_url
 
-    def should_be_login_form(self):
-        login_form = self.browser.find_element(By.CSS_SELECTOR, "#login_form")
+    def should_be_login_page(self):
+        login_page = self.browser.find_element(By.CSS_SELECTOR, "#login_form")
         assert True
 
-    def should_be_register_form(self):
-        register_form = self.browser.find_element(By.CSS_SELECTOR, "#register_form")
+    def should_be_register_page(self):
+        register_page = self.browser.find_element(By.CSS_SELECTOR, "#register_form")
         assert True
